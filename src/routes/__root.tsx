3d613228202bdc076129d13f48644537fa0bcf44
ShadowCatP <282276@student.pwr.edu.pl>
2026-06-07 12:48:30 +0200

    chore: remove tag colors and add theme provider

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,11 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 
+import { ThemeProvider } from "@/components/theme-provider";
+
 export const Route = createRootRoute({
-  component: () => <Outlet />,
+  component: () => (
+    <ThemeProvider>
+      <Outlet />
+    </ThemeProvider>
+  ),
 });
